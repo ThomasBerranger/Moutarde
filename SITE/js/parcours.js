67c5174688b0e8c65ab008a47564462805d36183
Naenaesean <Sean@MacBook-Pro-de-Sean.local>
2017-04-03 16:17:28 +0200

    Sean parcours
    pas fini

---
+++ SITE/js/parcours.js
@@ -0,0 +1,4 @@
+<script type="text/javascript" src="<?php echo tz_JS . '/bootstrap/jquery.js'; ?>"></script>
+
+<script type="text/javascript" src="<?php echo tz_JS . '/bootstrap/bootstrap-carousel.js'; ?>"></script>
+<script type="text/javascript" src="<?php echo tz_JS . '/bootstrap/bootstrap-transition.js'; ?>"></script>
